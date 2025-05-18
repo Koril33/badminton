704fa99ef854163709a7c84c32a9ab35c2935151
dingjinghui <dingjinghui33@163.com>
2025-05-18 17:58:35 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 启动 main.py 并记录 PID
-nohup ~/project/python/badminton/venv/bin/python main.py > /dev/null 2>&1 &
+nohup ~/project/badminton/venv/bin/python main.py > /dev/null 2>&1 &
 echo $! > pid.txt
 
 echo "服务已启动，进程ID: $(cat pid.txt)"
